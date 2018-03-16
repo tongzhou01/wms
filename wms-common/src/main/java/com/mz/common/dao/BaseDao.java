391a50830dbbd96c60f1ff22e636e4e76122bc42
tongzhou01 <568309196@qq.com>
2018-03-16 16:59:21 +0800

    基础basedao泛型修改

--- wms-common/src/main/java/com/mz/common/dao/BaseDao.java
+++ wms-common/src/main/java/com/mz/common/dao/BaseDao.java
@@ -2,11 +2,13 @@ package com.mz.common.dao;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 /**
  * BaseDao 数据访问层通用接口<br>
  * Created by tongzhou on 2017-09-12.
  */
 @Repository
-public interface BaseDao<T> extends IDao<T> {
+public interface BaseDao extends IDao<Map<String, Object>> {
 
 }
