ec90486159ddd7d185c4515d0e3e59d082f9ec19
tongzhou01 <568309196@qq.com>
2018-03-30 17:55:29 +0800

    处理余额

--- wms-admin/src/main/java/com/mz/admin/service/CustomerInfoService.java
+++ wms-admin/src/main/java/com/mz/admin/service/CustomerInfoService.java
@@ -1,11 +1,16 @@
 package com.mz.admin.service;
 
 import com.mz.admin.entity.CustomerInfo;
+import com.mz.common.entity.R;
 import com.mz.common.service.IService;
+
+import java.math.BigDecimal;
 
 public interface CustomerInfoService extends IService<CustomerInfo> {
 
     Integer selectMaxNumber();
 
     CustomerInfo selectByCustomerNo(String customerNo);
+
+    R dealBalance(Long id, BigDecimal money, Integer type);
 }
